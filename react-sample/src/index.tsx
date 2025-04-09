6767f91f387d22cfd82129da44968e4c8ca81791
B1ue-Te4 <103427649+B1ue-Te4@users.noreply.github.com>
2025-04-09 03:56:51 +0000

    04091256

--- react-sample/src/index.tsx
+++ react-sample/src/index.tsx
@@ -2,8 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 // import App from './App';
-import Hello from './components/Hello'
-import Name from './components/Name'
+// import Hello from './components/Hello'
+// import Name from './components/Name'
+import Message from './components/Message'
+import Parent from './components/ContainerSample'
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(
@@ -11,8 +13,8 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <React.StrictMode>
-    <Hello />
-    <Name />
+    <Message />
+    <Parent />
   </React.StrictMode>
 );
 
