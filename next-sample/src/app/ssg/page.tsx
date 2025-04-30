9678b29dfa11bf62307fb2a1f361b49a74e57c94
B1ue-Te4 <103427649+B1ue-Te4@users.noreply.github.com>
2025-04-30 12:54:05 +0000

    nextjs ssg

---
+++ next-sample/src/app/ssg/page.tsx
@@ -0,0 +1,36 @@
+import { GetStaticProps, NextPage, NextPageContext } from 'next'
+import Head from 'next/head'
+
+type SSGProps = {
+  message: string
+}
+
+const SSG: NextPage<SSGProps> = (props) => {
+  const { message } = props
+  return (
+    <div>
+      <Head>
+        <title>Static Site Generation</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main>
+        <p>
+          このページは静的サイト生成によってビルド時に生成されたページです
+        </p>
+      </main>
+    </div>
+  )
+}
+
+export const getStaticProps: GetStaticProps<SSGProps> = async(contxt) => {
+  const timestamp = new Date().toLocaleString()
+  const message = `${timestamp} に GetStaticProps が実行されました`
+  console.log(message)
+  return {
+    props: {
+      message,
+    },
+  }
+}
+
+export default SSG
